2dc9d4b5e2222e7a3eef74f89d0bdf3312306ae3
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-16 16:41:20 +0200

    onclick increase score and after specific score allow to purchase cookie, add multiplier

--- src/js/script.js
+++ src/js/script.js
@@ -5,15 +5,23 @@
  */
 
 (function() {
-let labelData = document.getElementById("cookieLabel");
+    let labelData = document.getElementById("cookieLabel");
     let counter = 0;
-    // your code here
+    let score = 0;
+        // your code here
    document.getElementById("run").addEventListener("click",()=>{
     
-    labelData.innerHTML = counter++;
-    
-    //+" tyep of counter = "+typeof(counter));
-
+    labelData.innerHTML = ++counter;
+    console.log("cnt = "+counter);
+   });
+   //Purchase cookie
+   document.getElementById("purchase").addEventListener("click",()=>{
+    if(counter >= 30) {
+        document.getElementById("purchaseCookie").innerHTML = score + 1;
+        counter = counter - 30;
+        cookieLabel.innerHTML = counter;
+    }
+    document.getElementById("scorePurchase").innerHTML = 30;
    });
     
 
